b5915780e7591c515e252b5ff5c1104ddd30a379
D-Dursty <devondahlia@gmail.com>
2022-12-06 13:41:12 -0800

    changes dataType to TEXT on description

--- models/Comment.js
+++ models/Comment.js
@@ -10,7 +10,7 @@ Comment.init(
             allowNull: false,
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull:false,
         },
         image: {
